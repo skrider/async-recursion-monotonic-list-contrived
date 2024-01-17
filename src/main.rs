a95b6bd1a8af26c2295982d8fb01b64027efa696
skrider <skrider@berkeley.edu>
2024-01-16 18:00:08 -0800

    parse arg

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let mut args = std::env::args();
+    let depth: i32 = args.nth(1).unwrap().parse().unwrap();
+    println!("Hello, world! {}", depth);
 }
